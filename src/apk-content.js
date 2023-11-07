8875e911de42393ab52a253b0a1f31360fe0de53
Sven65 <Sven65@users.noreply.github.com>
2023-11-07 17:59:01 +0100

    Fix extension

--- src/apk-content.js
+++ src/apk-content.js
@@ -11,43 +11,97 @@ const calculateAPK = (price, alcoholPercentage, volume) => {
 	return volume * volumePercentage / price
 }
 
+const createElementFromHTML = (htmlString) => {
+	const div = document.createElement('div')
+	div.innerHTML = htmlString.trim();
+  
+	return div.firstChild
+  }
+
+const hasSizeSelector = () => {
+	const sizeSelector = document.querySelector('.css-vmong3.e5tdxoe0')
+	
+	return !!sizeSelector
+}
+
+const getSizeDetails = () => {
+	const wineSelector = document.querySelector('.css-vmong3.e5tdxoe0')
+	const singleSize = document.querySelector('#__next > main > div.css-1c21j0p.e18roaja0 > div > div.css-h0miy6.e18roaja0 > div.css-tx0xzd.e17wolzc0 > div.css-1hho7ub.erf25qm0 > div > div.css-dahppd.e18roaja0 > div.css-1f5wlkv.e18roaja0 > div.css-1yfm6cm.e18roaja0 > p:nth-child(3)')
+
+
+	if (!!wineSelector) {
+		return wineSelector.options[0].innerHTML
+	}
+
+	return singleSize.innerHTML
+}
+
+const getCostContainerPath = () => {
+	return hasSizeSelector()
+		? '#__next > main > div.css-1c21j0p.e18roaja0 > div > div.css-h0miy6.e18roaja0 > div.css-tx0xzd.e17wolzc0 > div.css-1hho7ub.erf25qm0 > div > div.css-dahppd.e18roaja0 > div.css-1df247k.e18roaja0 > p'
+		: '#__next > main > div.css-1c21j0p.e18roaja0 > div > div.css-h0miy6.e18roaja0 > div.css-tx0xzd.e17wolzc0 > div.css-1hho7ub.erf25qm0 > div > div.css-dahppd.e18roaja0 > div.css-1df247k.e18roaja0 > p'
+}
+
+
+const getPercentageContainerPath = () => {
+	return hasSizeSelector()
+		? '#__next > main > div.css-1c21j0p.e18roaja0 > div > div.css-h0miy6.e18roaja0 > div.css-tx0xzd.e17wolzc0 > div.css-1hho7ub.erf25qm0 > div > div.css-dahppd.e18roaja0 > div.css-1f5wlkv.e18roaja0 > div.css-1yfm6cm.e18roaja0 > p'
+		: '#__next > main > div.css-1c21j0p.e18roaja0 > div > div.css-h0miy6.e18roaja0 > div.css-tx0xzd.e17wolzc0 > div.css-1hho7ub.erf25qm0 > div > div.css-dahppd.e18roaja0 > div.css-1f5wlkv.e18roaja0 > div.css-1yfm6cm.e18roaja0 > p:nth-child(5)'
+}
+
+const getDetails = () => {
+	const percentageContainer = document.querySelector(getPercentageContainerPath())
+	const costContainer = document.querySelector(getCostContainerPath())
+
+	const size = getSizeDetails()
+
+	return {
+		percentage: percentageContainer.innerHTML,
+		cost: costContainer.innerHTML,
+		size,
+	}
+
+	return deets
+}
+
+const normalizeUnits = (details) => {
+	const sizeRegex = /(\d+) ml/g
+
+	const matchedSize = details.size.match(sizeRegex)[0]
+
+	const normalizedAlcoholPercentage = parseFloat(details.percentage.replace(/\s?%\svol./g, '').replace(/,/, '.'))
+	const normalizedCost = parseFloat(details.cost.replace(/:/, '.'))
+	const normalizedSize = parseInt(matchedSize.replace('ml', '').replace(/\s/g, ''))
+
+	return {
+		percentage: normalizedAlcoholPercentage,
+		cost: normalizedCost,
+		size: normalizedSize
+	}
+}
+
+const insertAPKNode = (apk) => {
+	const parentNode = document.querySelector('#__next > main > div.css-1c21j0p.e18roaja0 > div > div.css-h0miy6.e18roaja0 > div.css-tx0xzd.e17wolzc0 > div.css-1hho7ub.erf25qm0 > div > div.css-dahppd.e18roaja0 > div:nth-child(8)')
+
+	const insertNode = `
+		<div class="css-1v0ksmu e18roaja0">
+			<p class="css-zehasz e1hb4h4s0">APK</p>
+			<p class="css-zehasz e1hb4h4s0">${apk.toFixed(3)}</p>
+		</div>
+	`
+
+	const elementAfter = parentNode.children[1]
+
+	parentNode.insertBefore(createElementFromHTML(insertNode), elementAfter)
+
+}
+
 const makeAPK = () => {
-	const parser = new DOMParser();
+	const details = normalizeUnits(getDetails())
 
-	fetch(window.location.toString()).then(r => r.text()).then(content => {
-		const doc = parser.parseFromString(content, "text/html")
+	const apk = calculateAPK(details.cost, details.percentage, details.size)
 
-		const detailPageContainer = doc.querySelector("[data-react-component=ProductDetailPageContainer]")
-
-		const productDetails = JSON.parse(detailPageContainer.dataset.props)
-
-		const { priceInclVat, alcoholPercentage, volume } = productDetails.product
-
-		const apk = calculateAPK(priceInclVat, alcoholPercentage, volume)
-
-		const smallDetailDisplayEval = document.evaluate("/html/body/div[1]/div[2]/main/div[1]/div[1]/div/div[1]/div[3]/div[1]/div/div[1]/div[4]/div/p[2]", document, null, XPathResult.ANY_TYPE, null)
-
-		const smallDetailDisplayNode = smallDetailDisplayEval.iterateNext()
-
-		// const dotDisplayEval = document.evaluate("/html/body/div[1]/div[2]/main/div[1]/div[1]/div/div[1]/div[3]/div[1]/div[2]/div[1]/div[3]/div[1]/p[2]", document, null, XPathResult.ANY_TYPE, null)
-		// const textDisplayEval = document.evaluate("/html/body/div[1]/div[2]/main/div[1]/div[1]/div/div[1]/div[3]/div[1]/div[2]/div[1]/div[3]/div[1]/p[3]", document, null, XPathResult.ANY_TYPE, null)
-
-		// const dotDisplayNode = dotDisplayEval.iterateNext()
-		// const textDisplayNode = textDisplayEval.iterateNext()
-
-		// const dotDisplayClone = dotDisplayNode.cloneNode(true)
-
-		// const apkDisplay = textDisplayNode.cloneNode(true)
-
-		const apkDisplay = smallDetailDisplayNode.cloneNode(true)
-
-		apkDisplay.innerHTML = `${apk.toFixed(3)} APK`
-
-		// textDisplayNode.parentElement.appendChild(dotDisplayClone)
-		smallDetailDisplayNode.parentElement.appendChild(apkDisplay)
-
-	})
-
+	insertAPKNode(apk)
 }
 
 makeAPK()
